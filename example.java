14768c479dd3df7084447d74e21a48db911e0563
cho hyo young <cluechy@gmail.com>
2020-09-06 14:32:22 +0900

    Problem1

--- example.java
+++ example.java
@@ -1,6 +1,21 @@
+import java.util.ArrayList;
+
 public class example {
 
     public static void main(String[] args) {
-        System.out.println("HELLO");
+        // for (int i = 1; i < 10; i++) {
+        // for (int k = 1; k <= 10; k++) {
+        // System.out.print(i * k+ " ");
+        // }
+        // System.out.println();
+
+        // }
+        int i = 1;
+        while (i <= 10) 
+           { System.out.println(i++);
+            System.out.println("hello");
+           }
+            System.out.println("hello");
+        
     }
 }
