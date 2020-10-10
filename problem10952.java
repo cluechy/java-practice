ea5020491c26a4d71d13e38450217e96c25fb5c6
cho hyo young <cluechy@gmail.com>
2020-10-10 16:26:06 +0900

    addsd

---
+++ problem10952.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class problem10952 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            while(ture) {
+                int N = scan.nextInt();
+                int M = scan.nextInt();
+                if(N==0 && M==0)
+                break;
+                System.out.println(N + M);
+            }
+}
+}
